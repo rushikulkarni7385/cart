39674a630052aae4fac2f4f9ace31286fb5ff0de
Rushikesh Kulkarni <rushikulkarni7385@gmail.com>
2022-01-13 22:55:28 +0530

    TO-2000

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -12,7 +12,6 @@ export class CartComponent implements OnInit, OnChanges {
   constructor(private cartService: CartServiceService, private http: HttpClient) { }
 
   ngOnInit(): void {
-    this.products = this.cartService.getItem();
   }
 
 
